17ecdd4d7ac5714cc7aea1ad5f217ad3a4b7598b
Bhabishya Ghimire <80686616+Bhabishya-123@users.noreply.github.com>
2023-07-15 08:48:36 +0545

    Update stripeConfig.php

--- stripeConfig.php
+++ stripeConfig.php
@@ -1,9 +1,9 @@
 <?php
 include_once('./stripe/init.php');
 
-$publishableKey="pk_test_51MH1kMSHriqitrpZ9wzffFCtudBTZqeAWtNlloNOUSdgGVhYV4HzBVPbmKsX4lOGGt2J2eSfbpP9IZTyoFWfYUxv0056vxnuZt";
+$publishableKey="pk_test_Your_Stripe_Publishable_Key";
 
-$secretKey="sk_test_51MH1kMSHriqitrpZHbyROzSGPYhFa9ZU0kgjZ7COByMl1YDPrDfcdh7HaXgpetYw2PQOWWcMpTfhKPJr2jlnrbMk00Z6NGsJ30";
+$secretKey="sk_test_Your_Stripe_Secret_Key";
 
 \Stripe\Stripe::setApiKey($secretKey);
 ?>
